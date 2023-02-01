b5047d015252f8f9bf27c7275328c03037be48ca
Bravestone <gauravmalimj@gmail.com>
2023-02-01 11:04:11 +0530

    Initial Commit

---
+++ SwiftUI-Wheather/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI-Wheather
+//
+//  Created by gaurav mali on 01/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Wheather/SwiftUI_WheatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WheatherApp.swift
+//  SwiftUI-Wheather
+//
+//  Created by gaurav mali on 01/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WheatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
